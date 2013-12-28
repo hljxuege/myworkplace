b35630e46edb76f31c1653aea280a53e38c16c77
liuxue <liuxue@liuxue-ThinkPad-T520>
2013-12-28 17:36:01 +0800

    rabbitmq tutorials 4 demo

---
+++ mypractice/rabbitmq4log/send.py
@@ -0,0 +1,23 @@
+#encoding:utf-8
+'''
+Created on Dec 28, 2013
+
+@author: liuxue
+'''
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs',
+                         type='direct')
+
+severity = 'info'
+message = 'Hello World!'
+channel.basic_publish(exchange='direct_logs',
+                      routing_key=severity,
+                      body=message)
+print " [x] Sent %r:%r" % (severity, message)
+connection.close()
